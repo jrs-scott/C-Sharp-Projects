83710a7442d42e087a87cb92996eacb2149f1211
Jamie Scott <north.jrn@gmail.com>
2019-04-15 12:30:14 -0700

    Console app demonstrating Boolean operators

---
+++ insuranceApproval/insuranceApproval/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace insuranceApproval
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the car insurance approval page. Please enter the follow information...");
+            Console.WriteLine("What is your age? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Please enter \"true\" or \"false\".");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have? ");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            bool approval = (age > 15 && dui == false && tickets <= 3);
+            Console.WriteLine("Are you qualified? \n" + approval);
+            Console.Read();
+        }
+    }
+}
