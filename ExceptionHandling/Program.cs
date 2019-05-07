e042f5a1ee78c4a53bdd412c4b07efec028a03c3
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-06 21:44:18 -0700

    Simple console app to show exception handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Let's do some division. Enter a number:");
+                int inputNumber = Convert.ToInt32(Console.ReadLine());
+                List<int> numbers = new List<int> { 12, 65, 92, 43, 70 };
+                foreach (int num in numbers)
+                {
+                    Console.WriteLine(num + " divided by " + inputNumber + " equals: " + (num / inputNumber));
+                }                    
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You did not enter an integer.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You can't divide by zero.");
+            }
+            finally
+            {
+                Console.WriteLine("The program has come to an end.");
+            }
+            Console.Read();
+        }
+    }
+}
