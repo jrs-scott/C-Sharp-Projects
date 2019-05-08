dde99c0ac9f9d04eea7dd7ab77e5aa235ee4d145
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-08 15:37:59 -0700

    Method overload and casting drill

---
+++ methodOverloadExample/Math.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace parameterExample
+{
+    public class Math
+    {
+        public static void Calculate(int input)
+        {
+            int value = input * 2;
+            Console.WriteLine("Int to int... {0} x 2 = {1}", input, value);
+        }
+
+        public static void Calculate(decimal input)
+        {
+            int value2 = Decimal.ToInt32(input + 10);
+            Console.WriteLine("Decimal to int... {0} + 10 = {1}", input, value2);
+        }
+
+        public static void Calculate(string input)
+        {
+            int value3 = Convert.ToInt32(input) - 2;
+            Console.WriteLine("String to int... {0} - 2 = {1}", input, value3);
+        }
+    }
+}
---
+++ methodOverloadExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace parameterExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("No user input required. Here are a few math operations.");
+
+            Math mathInt = new Math();
+            Math.Calculate(12);
+
+            Math mathDec = new Math();
+            Math.Calculate(5.6m);
+
+            Math mathString = new Math();
+            Math.Calculate("8");
+
+            Console.Read();
+        }
+    }
+}
