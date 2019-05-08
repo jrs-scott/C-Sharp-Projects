271ea97928a29488e2134d31114ed6ae3d17626c
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-08 12:39:39 -0700

    Drill using method parameters

---
+++ parameterExample/Math.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace parameterExample
+{
+    public class Math
+    {
+        public static void Multiply(int input, int input2)
+        {            
+            int result = input * 42; // Execute a math operation on the first input
+            Console.WriteLine("{0} x 42 = {1}", input, result);
+            Console.WriteLine("The second input you provided was: {0}", input2); // Display the second input provided
+        }
+    }
+}
---
+++ parameterExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace parameterExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            Math math = new Math(); // Instantiate the class
+            Math.Multiply(input, input2); // Pass in two numbers
+            Math.Multiply(input: 5, input2: 10); // Pass in two numbers, specifying the parameters by name
+
+            Console.Read();
+        }
+    }
+}
