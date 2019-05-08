bef3cb161cca00cfd38ab5296db97b83d7e5ce15
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-08 10:59:12 -0700

    Custom class with multiple methods
    Drill assignment for C#

---
+++ methodsExample/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace methodsExample
+{
+    public class Math
+    {
+        public static void Add(int input)
+        {            
+            int value = input + 50;
+            Console.WriteLine(input + " + 50 = " + value);
+        }
+        public static void Divide(int input)
+        {
+            int value = input / 10;
+            Console.WriteLine(input + " / 10 = " + value);
+        }
+        public static void Multiply(int input)
+        {
+            int value = input * 2;
+            Console.WriteLine(input + " x 2 = " + value);
+        }
+    }
+}
---
+++ methodsExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace methodsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a whole number: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+                Math.Add(input);
+                Math.Divide(input);
+                Math.Multiply(input);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }        
+    }
+}
