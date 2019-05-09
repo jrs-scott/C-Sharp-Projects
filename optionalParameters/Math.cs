8309df75deca70fa036b1c6fd107d65a65a1b20a
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-08 18:41:45 -0700

    Custom class example using an optional parameter

---
+++ optionalParameters/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalParameters
+{
+    public class Math
+    {
+        public static int Add(int number1, int number2 = 0)
+        {
+            int total = number1 + 50 + number2;
+            return total;
+        }
+    }
+}
---
+++ optionalParameters/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a whole number: ");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a second number. Otherwise, press enter: ");
+                string input = Console.ReadLine();
+                Math math = new Math();
+                if (input != "")
+                {
+                    int number2 = Convert.ToInt32(input);
+                    int total = Math.Add(number1, number2);
+                    Console.WriteLine("{0} + 50 + {1} = {2}", number1, number2, total);
+                }
+                else
+                {
+                    int total = Math.Add(number1);
+                    Console.WriteLine("{0} + 50 = {1}", number1, total);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
