cc32f956414eaf637505d62947fb9dfaf4aaff0a
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-08 19:20:12 -0700

    Custom class with an overloaded method

---
+++ simpleClass/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace simpleClass
+{
+    public class Math
+    {
+        public static void Divide(int dividend, out int total)
+        {
+            total = dividend / 2;
+        }
+
+        public static void Divide(int dividend, int divisor, out int total)
+        {
+            total = dividend / divisor;
+        }
+    }
+}
---
+++ simpleClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace simpleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to be divided: ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Math math = new Math();
+            int total = 0;
+            Math.Divide(dividend, out total);
+            Console.WriteLine("{0} / 2 = {1}", dividend, total);
+
+            Console.WriteLine("Please enter a number to be divided: ");
+            dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a number to divide by: ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            Math.Divide(dividend, divisor, out total);
+            Console.WriteLine("{0} / {1} = {2}", dividend, divisor, total);
+
+            Console.Read();
+        }
+    }
+}
