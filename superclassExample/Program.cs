181184c1e37949488cf4200aaf2205b661845d36
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-09 22:19:27 -0700

    Simple console app using a superclass method

---
+++ superclassExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace superclassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A short coding example using a superclass method.");
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+            employee.SayName();
+            Console.Read();
+        }
+    }
+}
