1b8b66e34a77211f475e2e0b6fca2e1b7887d5a3
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-12 22:59:28 -0700

    Overloading == and != operators

---
+++ operatorOverloadExample/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatorOverloadExample
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+
+        public static bool operator ==(Employee a, Employee b)
+        {
+            return a.Id.Equals(b.Id);
+        }
+
+        public static bool operator !=(Employee a, Employee b)
+        {
+            return !a.Id.Equals(b.Id);
+        }
+    }
+}
---
+++ operatorOverloadExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatorOverloadExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A short coding example of overloading an operator.");
+            Employee employee = new Employee() { FirstName = "Bob", LastName = "Smith", Id = 1 };
+            Employee employee2 = new Employee() { FirstName = "John", LastName = "Doe", Id = 2 };
+
+            Console.WriteLine("{0} and {1} have the same ID number: {2}", (employee.FirstName), (employee2.FirstName), (employee == employee2));
+
+            Console.Read();
+        }
+    }
+}
