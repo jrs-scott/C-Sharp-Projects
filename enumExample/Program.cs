fe328c0300b85ec8c7cb73561279c4a21789726d
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-14 10:18:05 -0700

    Drill 134: Check user input against an enum

---
+++ enumExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace enumExample
+{
+    public enum DaysOfTheWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter the current day of the week: ");
+            string userInput = Console.ReadLine();
+            try 
+            {
+                DaysOfTheWeek currentDay = (DaysOfTheWeek) Enum.Parse(typeof(DaysOfTheWeek), userInput, true);
+                Console.WriteLine("\nToday is {0}.", currentDay);                                              
+            }
+            catch(ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.Read();
+            }                     
+        }
+    }
+}
