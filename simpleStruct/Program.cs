2c586edf54c52549d7450d965c49b6d289968ecb
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-14 10:46:42 -0700

    Drill 137: Create a simple struct

---
+++ simpleStruct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleStruct
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number testNumber = new Number { Amount = 12.45m };
+            Console.WriteLine("The stored amount is: {0}", Convert.ToString(testNumber.Amount));
+            Console.Read();
+        }
+    }
+}
