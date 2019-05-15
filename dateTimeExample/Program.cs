d9495905f56b46a0cfb90ed6ea674fbd6122f9a3
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-15 16:19:12 -0700

    Add user input hours to the current date/time

---
+++ dateTimeExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dateTimeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The current date and time is {0}. \nPlease enter a number:", DateTime.Now);
+            double userNumber = Convert.ToDouble(Console.ReadLine());
+            DateTime futureTime = DateTime.Now.AddHours(userNumber);
+            Console.WriteLine("In {0} hours, it will be {1}.", userNumber, futureTime);
+            Console.Read();
+
+        }
+    }
+}
