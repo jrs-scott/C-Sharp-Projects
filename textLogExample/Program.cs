d06b080ccc71896f9058acc974397675546e30d4
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-15 15:57:22 -0700

    Log user input to a text file

---
+++ textLogExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace textLogExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            string userInput = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Jamie\Desktop\log.txt", true))
+            {
+                file.WriteLine(userInput);
+            }
+            Console.WriteLine("Your number has been logged. Thank you!");
+            Console.Read();
+        }
+    }
+}
