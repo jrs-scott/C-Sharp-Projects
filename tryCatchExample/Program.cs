c3e1e8fe018fcc7da6535a40ce0a9b8d7944cecb
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-15 21:20:19 -0700

    Drill 165: Implement a try catch block

---
+++ tryCatchExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatchExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            try
+            {
+                Console.WriteLine("Please enter your age:");
+                int.TryParse(Console.ReadLine(), out int age);
+                if (age <= 0)
+                {
+                    throw new ArgumentException();
+                }    
+                int birthYear = DateTime.Now.Year - age;
+                Console.Write("Unless your birthday is later this year, you were born in {0}.", birthYear);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("You entered an invalid input. Please restart the program and try again.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error has occurred. Please restart the program and try again.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
