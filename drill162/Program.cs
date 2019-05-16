1d5edd0e7b1c57127db183b45001b4c048919249
Jamie Scott <47962101+jrs-scott@users.noreply.github.com>
2019-05-15 19:50:04 -0700

    Use a constant, variable, and constructors

---
+++ drill162/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drill162
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            const string constantName = "This string is a constant."; // 1) Create a constant variable
+            var num = 42; // 2) Use the "var" keyword in a new variable
+            Person newPerson = new Person("John");
+            Console.WriteLine("{0}\nThe integer value is {1}.\n{2} is a new Person.", constantName, num, newPerson.FirstName);
+            Console.Read();
+        }
+    }
+    public class Person
+    {
+        public Person(string fName) : this(fName, "", 00) // 3) Chain two constructors together
+        {
+        }
+        public Person(string fName, string lName, int age)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = age;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
